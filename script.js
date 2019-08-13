52351b4818eb944fc7c0369cb07148a877af9d5f
denise <denise.mai21@gmail.com>
2019-08-12 21:31:40 -0400

    part two

--- script.js
+++ script.js
@@ -3,40 +3,46 @@ const user = "";
 
 
 
-//This will prompt to start the game
+//This function will prompt to start the game
+function startGame() {
   const play = prompt("Would you like to play?").toLowerCase();
-  while (play == "no"){
+  while (play === "no"){
     console.log("See you next time.");
   }
   if (play == "yes"){
-    name1 = prompt("Enter your name.").toLowerCase();
+    name1 = prompt("Enter your name.");
+  startCombat();
   }
-  if (enemy !== true){
-    console.log("Ready! Let the battle begin!");
-  }
-
+}
 
 //Function get damage
 function getDamage(){
-  return Math.floor((Math.random() * 2) + 1);
+  return Math.floor((Math.random() * 5) + 1);
 }
 
 //Randomize health points
-  const playing = true;
-  let userHealth = 40;
-  let enemyHealth = 10;
-  let wins = 0;
+const playing = true;
+let userHealth = 40;
+let enemyHealth = 10;
+let wins = 0;
 
-  while (play) {
+function startCombat() {
+  
+  while (playing) {
     if (wins === 3) {
       console.log("You won!!");
       break;
     }
-    if (play === "yes"){
+    let choice = prompt("Do you want to attack or quit?").toLowerCase();
+    if (choice === "attack"){
       userHealth -= getDamage();
       enemyHealth -= getDamage();
       console.log(name1 + "'s health is now: " + userHealth + "!");
       console.log(enemy + "'s health is now: " + enemyHealth + "!");
+    }
+    else if (choice === "quit") {
+      console.log("That's too bad you lose! Better luck next time!");
+      break;
     }
     if (userHealth <= 0) {
       console.log("You lose, and " + enemy + " wins!");
@@ -44,7 +50,10 @@ function getDamage(){
     }
     if (enemyHealth <= 0) {
       wins++;
-      enemyHealth =10;
-      console.log("You won the fight!! " + enemy + "loses!");
+      enemyHealth =5;
+      console.log(`You won the fight!! ${enemy} loses! You have ${3 - wins} more round(s) left.`);
     }
   }
+}
+
+  startGame();
